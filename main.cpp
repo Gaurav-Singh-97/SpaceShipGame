00ffef3da417db4f4abc25d203fc25473ec49f0f
Gaurav-ISM <gauravthegeek@gmail.com>
2016-05-25 00:18:35 -0800

    Create view with rectangular item
    Created a QGraphicsScene, contained a QGraphicsRectItem and displayed it
    using QGraphicsView

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    // create a scene
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    // create an item to put into the scene
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect->setRect(0, 0, 100, 100);
+
+    scene->addItem(rect);
+
+    // add a view
+    QGraphicsView * view = new QGraphicsView(scene);
+    // or can use view->setScene(scene);
+    view->show();
 
     return a.exec();
 }
