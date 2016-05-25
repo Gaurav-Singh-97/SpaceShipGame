54df42e06c93f46023402bd12a13998b2f986fe0
Gaurav-ISM <gauravthegeek@gmail.com>
2016-05-25 01:41:45 -0800

    Add controls on MyRect
    Removed the debug message and added controls for arrow keys in MyRect

--- myrect.cpp
+++ myrect.cpp
@@ -1,7 +1,17 @@
 #include "myrect.h"
-#include <QDebug>
+#include <QKeyEvent>
 
 void MyRect::keyPressEvent(QKeyEvent *event)
 {
-    qDebug() << "You just pressed a key...";
+    if (event->key() == Qt::Key_Left)
+        setPos(x()-10, y());
+    else
+    if (event->key() == Qt::Key_Right)
+        setPos(x()+10, y());
+    else
+    if (event->key() == Qt::Key_Up)
+        setPos(x(), y()-10);
+    else
+    if (event->key() == Qt::Key_Down)
+        setPos(x(), y()+10);
 }
